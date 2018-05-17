3ce7f44a0b56ea88eacff325da38456adb67a043
MistaMek <grigor1ev.vladimir@yandex.ru>
2018-05-17 23:33:16 +0300

    added layer drawable

--- src/main/kotlin/drawables/LayerDrawable.kt
+++ src/main/kotlin/drawables/LayerDrawable.kt
@@ -1,22 +1,47 @@
 package drawables
 
+import android.graphics.drawable.LayerDrawable
 import org.w3c.dom.Element
 
 class LayerDrawable : DrawableWrapper() {
 
-    private var insetLeft = 0
-    private var insetTop = 0
-    private var insetRight = 0
-    private var insetBottom = 0
-    private var insetStart = 0
-    private var insetEnd = 0
+    companion object {
+        private const val PADDING_TOP = "android:paddingTop"
+        private const val PADDING_BOTTOM = "android:paddingBottom"
+        private const val PADDING_LEFT = "android:paddingLeft"
+        private const val PADDING_RIGHT = "android:paddingRight"
+        private const val PADDING_END = "android:paddingEnd"
+        private const val PADDING_START = "android:paddingStart"
 
-    private var width = 0
-    private var height = 0
+        private const val PADDING_MODE = "android:paddingMode"
+        private const val PADDING_MODE_STACK = "stack"
+    }
+
+    private var paddingLeft = 0
+    private var paddingTop = 0
+    private var paddingRight = 0
+    private var paddingBottom = 0
+
+    private var paddingMode = LayerDrawable.PADDING_MODE_NEST
 
     override fun inflate(element: Element) {
         super.inflate(element)
 
+        element.attributes?.also {
+            it.getNamedItem(PADDING_MODE)?.run {
+                paddingMode = when (this.nodeValue) {
+                    PADDING_MODE_STACK -> LayerDrawable.PADDING_MODE_STACK
+                    else -> LayerDrawable.PADDING_MODE_NEST
+                }
+            }
 
+            it.getNamedItem(PADDING_TOP)?.run { ParseUtils.parseAttributeAsInt(this) }?.also { paddingTop = it }
+            it.getNamedItem(PADDING_BOTTOM)?.run { ParseUtils.parseAttributeAsInt(this) }?.also { paddingBottom = it }
+            it.getNamedItem(PADDING_LEFT)?.run { ParseUtils.parseAttributeAsInt(this) }?.also { paddingLeft = it }
+            it.getNamedItem(PADDING_RIGHT)?.run { ParseUtils.parseAttributeAsInt(this) }?.also { paddingRight = it }
+
+            it.getNamedItem(PADDING_START)?.run { ParseUtils.parseAttributeAsInt(this) }?.also { paddingLeft = it }
+            it.getNamedItem(PADDING_END)?.run { ParseUtils.parseAttributeAsInt(this) }?.also { paddingRight = it }
+        }
     }
 }
--- src/main/kotlin/drawables/ParseUtils.kt
+++ src/main/kotlin/drawables/ParseUtils.kt
@@ -1,11 +1,17 @@
 package drawables
 
 import org.w3c.dom.Node
+import java.util.*
 
 class ParseUtils {
     companion object {
+
         fun parseAttributeAsInt(node: Node): Int {
-            return node.nodeValue?.toInt() ?: 0
+            return try {
+                Scanner(node.nodeValue).useDelimiter("\\D+").nextInt()
+            } catch (e: Exception) {
+                0
+            }
         }
     }
 }
