5a26c6ce098bc3d3b7ad74b15c8f3f22ce19d531
vlgrigorev <vlgrigorev@ozon.ru>
2018-09-12 20:48:33 +0300

    refactor

--- src/main/kotlin/com/mistamek/drawablepreview/factories/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.mistamek.drawablepreview.factories
-
-object Constants {
-    const val ICON_SIZE = 16
-    const val XML_TYPE = ".xml"
-    const val DRAWABLES_FOLDER_TYPE = "drawable"
-}
