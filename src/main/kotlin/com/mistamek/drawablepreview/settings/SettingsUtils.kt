aaf2e6fd5852bdeffd31cb7128318819b90d3a06
vlgrigorev <vlgrigorev@ozon.ru>
2018-11-13 21:34:47 +0300

    added invalidate to project view

--- src/main/kotlin/com/mistamek/drawablepreview/settings/SettingsUtils.kt
+++ src/main/kotlin/com/mistamek/drawablepreview/settings/SettingsUtils.kt
@@ -1,6 +1,8 @@
 package com.mistamek.drawablepreview.settings
 
+import com.intellij.ide.projectView.ProjectView
 import com.intellij.ide.util.PropertiesComponent
+import com.intellij.openapi.project.ProjectManager
 import com.mistamek.drawablepreview.factories.Constants
 
 object SettingsUtils {
@@ -15,5 +17,8 @@ object SettingsUtils {
 
     fun apply(previewSize: Int) {
         PropertiesComponent.getInstance().setValue(PROPERTIES_SIZE, previewSize, Constants.ICON_SIZE)
+        ProjectManager.getInstance().openProjects.forEach {
+            ProjectView.getInstance(it).refresh()
+        }
     }
 }
